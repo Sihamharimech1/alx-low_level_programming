e2aa8cd39cca1250f4fd1ea6affcc9bc472514a0
HARIMECH <sihamharimech00@gmail.com>
2023-03-28 18:41:19 -0700

    int n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*  reset_to_98 - takes the pointer and change la valeur
+*  @n: int to check
+*  i hate pointers
+*  Return: 0 is success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
