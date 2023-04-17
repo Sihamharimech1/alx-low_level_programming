09ac2f6556108c701b39eca07e5aba82cc33acd1
HARIMECH <sihamharimech00@gmail.com>
2023-04-17 16:42:41 -0700

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the name of the source file.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Name of source file: %s\n", __FILE__);
+
+	return (0);
+}
