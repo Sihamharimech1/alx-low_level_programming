8e9f426d7173fa2afd744425cdf71fb0e85d2bd9
HARIMECH <sihamharimech00@gmail.com>
2023-04-27 18:16:24 -0700

    fun

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - prints a message before main function execution
+ *
+ * Description: This function prints the message "You're beat! and yet, you must
+ * allow,\nI bore my house upon my back!\n" to the standard output stream.
+ * Return: Nothing
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
