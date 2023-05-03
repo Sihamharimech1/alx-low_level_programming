67c541710cc52cc2aea1399eefa5830f76daeadd
HARIMECH <sihamharimech00@gmail.com>
2023-05-02 17:50:20 -0700

    task 13 update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,5 +32,6 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
+void free_ptrs(listint_t **ptrs, size_t count);
 
 #endif /*LISTS_H*/
