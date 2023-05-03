dbf21fbb47ce15892c582c9cacf405b4736c1479
HARIMECH <sihamharimech00@gmail.com>
2023-05-02 17:48:43 -0700

    task 13 update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,33 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * free_listint_safe - Frees a listint_t list.
- * @h: A pointer to the address of the head of the list
+ * free_listint_safe - Frees a listint_t linked list.
+ * @head: Double pointer to the beginning of the list.
  *
- * Return: The size of the list that was free'd
- *         The function sets the head to NULL.
+ * Return: The size of the list that was freed.
  */
-size_t free_listint_safe(listint_t **h)
+size_t free_listint_safe(listint_t **head)
 {
 	size_t count = 0;
-	listint_t *temp;
+	listint_t *temp, **ptrs = NULL;
 
-	if (h == NULL)
-		return (0);
+	if (!head || !*head)
+		return (count);
 
-	while (*h != NULL)
+	while (*head)
 	{
-		count++;
-		if (*h >= (*h)->next)
+		ptrs = realloc(ptrs, (count + 1) * sizeof(*ptrs));
+		if (!ptrs)
 		{
-			free(*h);
-			*h = NULL;
-			break;
+			free_ptrs(ptrs, count);
+			exit(98);
 		}
-		temp = (*h)->next;
-		free(*h);
-		*h = temp;
+		for (size_t i = 0; i < count; i++)
+		{
+			if (*head == ptrs[i])
+			{
+				*head = NULL;
+				free_ptrs(ptrs, count);
+				return (count);
+			}
+		}
+		ptrs[count] = *head;
+		count++;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+	free_ptrs(ptrs, count);
 	return (count);
+}
+
+/**
+ * free_ptrs - Frees a pointer array.
+ * @ptrs: Pointer to the beginning of the array.
+ * @count: Number of elements in the array.
+ */
+void free_ptrs(listint_t **ptrs, size_t count)
+{
+	if (ptrs)
+	{
+		for (size_t i = 0; i < count; i++)
+			ptrs[i] = NULL;
+		free(ptrs);
+	}
 }
