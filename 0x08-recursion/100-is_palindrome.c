0bbc8566c04ae419bef31198416cb16c4ecbe801
HARIMECH <sihamharimech00@gmail.com>
2023-05-02 18:13:15 -0700

    task add 1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,21 +9,6 @@
  */
 int is_palindrome(char *s)
 {
-	int len = strlen(s);
-
-	if (len <= 1)
-	{
-		/* An empty string or a string of length 1 is a palindrome */
-		return (1);
-	}
-	else if (s[0] == s[len - 1])
-	{
-		/* The first and last characters match, so we recurse on the substring between them */
-		return (is_palindrome(s + 1));
-	}
-	else
-	{
-		/* The string is not a palindrome */
-		return (0);
-	}
+    int len = strlen(s);
+    return (len <= 1) ? 1 : (s[0] == s[len - 1]) ? is_palindrome(s + 1) : 0;
 }
